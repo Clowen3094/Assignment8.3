467e0b111d75e314641a39507d6978da54018750
Clowen3094 <kaninia207@gmail.com>
2017-06-26 11:45:15 +0530

    Add files via upload

---
+++ Assignment8.3-master/src/MyThread2.java
@@ -0,0 +1,11 @@
+//creating thread and passing deposit and withdraw amount through it
+class MyThread2 extends Thread{  
+	OnlineBanking t;  
+	MyThread2(OnlineBanking t){  
+	this.t=t;  
+}  
+public void run(){  
+		t.deposite(100);
+		t.withdraw(2000);
+	}  
+}
